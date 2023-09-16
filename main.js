55c27fbc22b2e474ccc30d3a0692aa5b04a09202
Sean Crooks <115957278+crooks-s@users.noreply.github.com>
2023-09-16 14:13:36 -0700

    Create field class with constructor and print()

--- main.js
+++ main.js
@@ -4,3 +4,16 @@ const hat = '^';
 const hole = 'O';
 const fieldCharacter = '░';
 const pathCharacter = '*';
+
+class Field {
+    constructor(field) {
+        this.field = field;
+    };
+
+    print(){
+        for(let i = 0; i < this.field.length; i++){
+            const joinSections = this.field[i].join('');
+            console.log(joinSections);
+        }
+    }
+};
